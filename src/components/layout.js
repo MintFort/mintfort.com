bb6c191e953b5f62fe580abd8c198d41fb2854d4
Axel <axelfuh@gmail.com>
2018-08-02 09:41:09 +0200

    Remove unused css (Normalize comes with Typography)

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
 
 const Layout = ({ children, data }) => (
   <StaticQuery
