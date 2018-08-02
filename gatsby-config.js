b963e043a4163dbc5732fd878957ebfd5e0ff101
Axel <axelfuh@gmail.com>
2018-08-02 12:47:37 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,22 @@
 module.exports = {
   siteMetadata: {
     title: 'Mintfort',
+    description: 'Be Yout Bank'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`
+      }
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-  ],
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    }
+  ]
 }
