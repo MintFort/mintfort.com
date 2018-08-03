5d0e23ed7103769bea63520eed0e36085fcc724f
Axel <axelfuh@gmail.com>
2018-08-03 11:31:26 +0200

    Remove unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Hero from '../components/Hero'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = ({ location }) => (
+  <Layout location={location}>
     <Hero language={'en'}/>
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Link to="/about">Go to About</Link>
-    </div>
   </Layout>
 )
 
