71c1b38281990b2317d7e3438b56e4fa8301eba8
Axel <axelfuh@gmail.com>
2018-08-07 09:13:57 +0200

    Add offline sw and google manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,31 @@ module.exports = {
     'gatsby-transformer-yaml',
     'gatsby-plugin-sitemap',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        description: config.siteDescription,
+        start_url: "/",
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: "minimal-ui",
+        icons: [
+          {
+            src: '/icons/mintfort_icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/mintfort_icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    },
+    "gatsby-plugin-offline",
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/data/`
