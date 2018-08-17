a3c5e3c1306ba50483078d7f8ec996edcefbee50
Axel <axelfuh@gmail.com>
2018-08-17 19:30:05 +0200

    Remove layout background img

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import styled, { injectGlobal } from "styled-components"
+import { injectGlobal } from "styled-components"
 import { css } from 'styled-components'
 import { configureAnchors } from 'react-scrollable-anchor'
 
@@ -11,8 +11,6 @@ import SEO from 'components/seo'
 import { Context, addLang } from 'components/context'
 
 import { theme, hover, transitions } from 'library/utils'
-
-import backImage from 'assets/svg/global_background-01.svg'
 
 configureAnchors({
   offset: -60,
@@ -38,11 +36,6 @@ injectGlobal`
   }
 `
 
-const Main = styled.main`
-  background-image: url(${backImage});
-  background-repeat: repeat-y;
-`
-
 const Layout = ({ children, location }) => (
   <StaticQuery
     query={graphql`
@@ -59,9 +52,9 @@ const Layout = ({ children, location }) => (
       <Context location={location}>
         {addLang(SEO, { path: location.pathname })}
         {addLang(Header, { siteTitle: data.site.siteMetadata.title, location })}
-        <Main>
+        <main>
           {children}
-        </Main>
+        </main>
         <Footer />
       </Context>
       </>
