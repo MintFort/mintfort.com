8b49e072eebbc9fdbe33e18e0ed50d22f5804121
Axel <axelfuh@gmail.com>
2018-08-17 19:33:22 +0200

    Hero basic position

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,31 +4,29 @@ import PropTypes from 'prop-types'
 // import { graphql, StaticQuery } from 'gatsby'
 import { FaChevronDown } from 'react-icons/fa'
 import { goToAnchor } from 'react-scrollable-anchor'
-import Fade from 'react-reveal/Fade'
-
+import { Button } from 'components/form'
 import styled, { css } from 'styled-components'
 import { hover, rem, transitions, navHeight, theme, flex, phone } from 'library/utils'
 
-import { Container, Title, SubHeader, Img } from 'library/index'
-import heroBackground from 'assets/svg/hero_background.svg'
-
-import Divider from 'library/divider'
+import { Container, Title, SubHeader, Img, Paragraph } from 'library/index'
+import heroBackground from 'assets/svg/hero-background.jpg'
 
 const Background = styled.section`
-  background: #eaedf1 url(${heroBackground});
-  background-position: center;
-  background-repeat: no-repeat;
+  background: url(${heroBackground});
   background-size: cover;
+  background-position: bottom;
+  background-repeat: no-repeat;
 
-  min-height: calc(100vh - ${navHeight});
+  height: 100vh;
   padding-top: ${navHeight};
+  position: relative;
 
   display: flex;
   flex-direction: column;
 `
 
 const Content = Container.extend`
-  padding: ${rem(80)} ${rem(60)} 0 ${rem(60)};
+  padding: ${rem(40)};
   display: flex;
   flex: 1;
 
@@ -38,16 +36,11 @@ const Content = Container.extend`
   `)}
 `
 
-const IconWrapper = Container.extend`
-  position: absolute;
-  bottom: 0;
-`
-
 const Icon = styled(FaChevronDown)`
-  width: ${rem(40)};
-  height: ${rem(40)};
+  width: ${rem(50)};
+  height: ${rem(50)};
   padding: ${rem(8)};
-  color: #fff;
+  color: ${theme.blue};
   cursor: pointer;
 
   ${hover(css`
@@ -56,17 +49,30 @@ const Icon = styled(FaChevronDown)`
 
   ${transitions('transform 0.2s ease-in')}
 `
+const IconWrapper = styled.div`
+  position: absolute;
+  bottom: 30px;
+  width: 100%;
+  ${flex}
+`
+
+const TextWrapper = styled.div`
+  flex: 7;
+`
+
+const ImageWrapper = styled.div`
+  flex: 5;
+  padding-right: ${rem(40)};
+  ${flex({ x: "flex-end", y: 'flex-start' })}
+`
 
 const Text = ({ title, subTitle, body }) => (
-  <div style={{ flex: 1 }}>
-    <Fade>
-      <SubHeader>{title}</SubHeader>
-      <Title size={70}>{subTitle}</Title>
-    </Fade>
-    <Fade delay={200}>
-      <p>{body}</p>
-    </Fade>
-  </div>
+  <TextWrapper>
+    <SubHeader color={theme.lightFont}>{title}</SubHeader>
+    <Title size={70} color="#fff">{subTitle}</Title>
+    <Paragraph style={{ color: '#fff' }}>{body}</Paragraph>
+    <Button onClick={() => goToAnchor("subscribe")}>Pre-Register</Button>
+  </TextWrapper>
 )
 
 Text.propTypes = {
@@ -74,11 +80,6 @@ Text.propTypes = {
   subTitle: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired
 }
-
-const ImageWrapper = styled.div`
-  flex: 1;
-  ${flex({ x: "center", y: 'flex-start' })}
-`
 
 const Image = ({ img, imgSize }) => (
   <ImageWrapper>
@@ -102,18 +103,19 @@ Image.propTypes = {
 const Hero = ({ title, subTitle, body, img, imgSize, scrollId }) => (
   <Background col>
     <Content>
+      <Image
+        img={img}
+        imgSize={imgSize}
+      />
       <Text
         title={title}
         subTitle={subTitle}
         body={body}
       />
-      <Image img={img} imgSize={imgSize}/>
     </Content>
-    <Divider fill={theme.blue}>
-      <IconWrapper centrate size={{ h: '100%', w: '100%' }}>
-        <Icon onClick={() => goToAnchor(scrollId)}/>
-      </IconWrapper>
-    </Divider>
+    <IconWrapper>
+      <Icon onClick={() => goToAnchor(scrollId)}/>
+    </IconWrapper>
   </Background>
 )
 
