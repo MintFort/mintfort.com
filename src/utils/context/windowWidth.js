25fbaef8e3caa9b1bcfdd77f4086e7a70a44eec1
Axel <axelfuh@gmail.com>
2018-10-31 14:03:51 +0100

    Just in case, maybe in the future we need it.

--- src/utils/context/windowWidth.js
+++ src/utils/context/windowWidth.js
@@ -2,6 +2,22 @@ import React, { Component, createContext } from 'react'
 
 const { Provider, Consumer } = createContext()
 
+// // Loadsh debounce function
+// function debounce(func, wait, immediate) {
+//   let timeout
+//   return function() {
+//     let context = this, args = arguments
+//     let later = function() {
+//       timeout = null
+//       if (!immediate) func.apply(context, args)
+//     }
+//     let callNow = immediate && !timeout
+//     clearTimeout(timeout)
+//     timeout = setTimeout(later, wait)
+//     if (callNow) func.apply(context, args)
+//   }
+// }
+
 export class WindowWidthProvider extends Component {
   state = {
     width: typeof window !== 'undefined' && window.innerWidth
