360547361172546acb8a260ea57d2e519fc7f2b9
Axel <axelfuh@gmail.com>
2018-11-15 23:20:12 +0100

    Add page query to Home and Portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { graphql } from "gatsby"
 
 import Layout from 'components/layout'
 import Home from 'components/home/home'
 
-const IndexPage = ({ location }) => (
+const IndexPage = ({ data: { page }, location }) => (
   <Layout location={location}>
-    <Home />
+    <Home data={page}/>
   </Layout>
 )
 
 IndexPage.propTypes = {
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 }
 
 export default IndexPage
+
+export const indexQuery = graphql`
+  query($local: String) {
+    page: contentfulPage(
+      title: { regex:"/home/i" },
+      node_locale: { regex: $local }
+    ) {
+      hero {
+        header
+        subheader
+        description {
+          md: childMarkdownRemark {
+            html
+          }
+        }
+        image {
+          fluid(maxWidth: 340) {
+            ...GatsbyContentfulFluid_noBase64
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { graphql } from "gatsby"
 
 import Layout from 'components/layout'
 import Portfolio from 'components/portfolio/portfolio'
 
-const PortfolioPage = ({ location }) => (
+const PortfolioPage = ({ data: { page }, location }) => (
   <Layout location={location}>
-    <Portfolio />
+    <Portfolio data={page}/>
   </Layout>
 )
 
 PortfolioPage.propTypes = {
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 }
 
 export default PortfolioPage
+
+export const indexQuery = graphql`
+  query($local: String) {
+    page: contentfulPage(
+      title: { regex:"/portfolio/i" },
+      node_locale: { regex: $local }
+    ) {
+      hero {
+        header
+        subheader
+        description {
+          md: childMarkdownRemark {
+            html
+          }
+        }
+        image {
+          fluid(maxWidth: 1350) {
+            ...GatsbyContentfulFluid_noBase64
+          }
+        }
+      }
+    }
+  }
+`
