794e66cc19d7f1094a9cb738e2555a628f4cca34
Axel <axelfuh@gmail.com>
2018-11-17 17:55:36 +0100

    Remove unused assets and code

--- siteConfig.js
+++ siteConfig.js
@@ -14,20 +14,8 @@ module.exports = {
     { name: "Impressum", path: '/impressum/', section: 'footer' },
     { name: "Privacy Policy", path: '/policy/', section: 'footer' }
   ],
-  social: [
-    { name: "Facebook", url: 'https://www.facebook.com/mintfortbank' },
-    { name: "Twitter", url: 'https://twitter.com/mintfort' },
-    { name: "Linkedin", url: 'https://www.linkedin.com/company/mintfort/' },
-    { name: "Medium", url: 'https://medium.com/mintfort' },
-    { name: "Github", url: 'https://github.com/MintFort' }
-  ],
-  contact: [
-    { city: 'Berlin', mail: 'berlin@mintfort.com', address: 'Hildegard-Marcusson-Str. 23, </br> 10317 Berlin </br> Germany', icon: 'icon-berlin.png' },
-    { city: 'Hong Kong', mail: 'hongkong@mintfort.com', address: 'Rm 1402, 14/F. </br> Hi Kwan Commercial Bldg., 38 Pitt Street </br> Kowloon </br> Hong Kong', icon: 'icon-hongkong.png' }
-  ],
   portfolio: {
     mac: 'https://download.mintfort.com/Mintfort-0.5.2.dmg',
     win: 'https://download.mintfort.com/Mintfort-0.5.2.exe'
-  },
-  copyright: '© 2018 Mintfort. All rights reserved. Be Your Bank.'
+  }
 }
