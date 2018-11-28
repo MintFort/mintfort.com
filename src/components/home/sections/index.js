cc3d26ffc885a6b68ffbd7567719afcc521ca5b1
Axel <axelfuh@gmail.com>
2018-11-28 09:33:46 +0100

    Refactor home sections

--- src/components/home/sections/index.js
+++ src/components/home/sections/index.js
@@ -1,6 +1,5 @@
 import Imagine from './sectionImagine'
 import Buy from './sectionBuy'
-import Phone from './sectionPhone'
 import Cards from './sectionCards'
 import Access from './sectionAccess'
 import Boxes from './sectionBoxes'
@@ -8,4 +7,4 @@ import Beta from './sectionBeta'
 import News from './sectionNews'
 import Form from './sectionForm'
 
-export { Imagine, Buy, Phone, Cards, Access, Boxes, Beta, News, Form }
+export { Imagine, Buy, Cards, Access, Boxes, Beta, News, Form }
